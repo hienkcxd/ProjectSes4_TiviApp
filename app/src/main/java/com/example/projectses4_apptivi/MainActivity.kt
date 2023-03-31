9d4657b65657df75af267a918a73631b9724cbe6
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2023-03-31 18:40:55 +0700

    update tivi app

--- app/src/main/java/com/example/projectses4_apptivi/MainActivity.kt
+++ app/src/main/java/com/example/projectses4_apptivi/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.projectses4_apptivi
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import com.example.projectses4_apptivi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener{
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.onTouchEvent(event)
     }
 }
